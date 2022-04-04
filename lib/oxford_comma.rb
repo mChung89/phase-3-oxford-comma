bcda8cb6bc59feb6f3141448873edb49ae79461f
Michael Chung <mikechung89@gmail.com>
2022-04-04 14:30:58 -0400

    commit tree 62c5e9bee6e536e4f7ebf13d1aad589d8e67db8c

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+    last_word = array.pop
+    word = ""
+    if array.length == 0
+        word = last_word
+    elsif array.length == 1
+        word = array[0] + " and " + last_word
+    elsif array.length > 1
+        word = array.join(", ") + ", and #{last_word}"
+    end
+    word
 end
